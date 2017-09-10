81fad724b17a817e019cf4ff1c709ce6d1c71d75
Markus Rickert <rickert@fortiss.org>
2017-09-10 15:20:01 +0200

    Fix redefinition error for math functions

--- src/PQP_Compile.h
+++ src/PQP_Compile.h
@@ -44,10 +44,6 @@
 // prevents compiler warnings when PQP_REAL is float
 
 #include <math.h>
-inline float sqrt(float x) { return (float)sqrt((double)x); }
-inline float cos(float x) { return (float)cos((double)x); }
-inline float sin(float x) { return (float)sin((double)x); }
-inline float fabs(float x) { return (float)fabs((double)x); }
 
 //-------------------------------------------------------------------------
 //
